6fa57284b02f3dec8a11855f49e901b1e85bee77
Farlox <5080040+Farlox@users.noreply.github.com>
2018-03-23 02:34:45 -0700

    Added several helper functions; Added INIT mode;  Added Science/Workshop

--- kittens.js
+++ kittens.js
@@ -1,6 +1,8 @@
 var k = {
 	mode: "on",
-	craftRatio: 10.0,
+	craftRatio: 1.0,
+	msg: '',
+	needs: [],
 	buildorder:
 	[
 		{ label: "Unic. Pasture", name: "" }, 
@@ -9,7 +11,7 @@ var k = {
 		{ label: "Lumber Mill", name: "lumberMill" }, 
 	    { label: "Aqueduct", name: "aqueduct" },
 		{ label: "Mine", name: "mine" },
-//      { label: "Quarry", name: "quarry" },
+        { label: "Quarry", name: "quarry" },
 		{ label: "Academy", name: "academy" },
 		{ label: "Library", name: "library" },
 		{ label: "Temple", name: "temple" },
@@ -24,6 +26,31 @@ var k = {
 	],
 	isFull: function(resName) {
 		return (gamePage.resPool.resourceMap[resName].value / gamePage.resPool.resourceMap[resName].maxValue) >= 0.95
+	},
+	canAfford: function(prices) {
+		var affordable = true;
+		prices.forEach(function(cost) {
+			if (gamePage.resPool.resourceMap[cost.name].value < cost.val) {
+				affordable = false;
+			}
+		});
+		return affordable;
+	},
+	build: function(bldLabel) {
+		var bld = $(".btnContent:contains(" + bldLabel + ")");
+		if (bld.length == 1 && !bld.parent().hasClass("disabled")) {
+			console.log("building " + bldLabel);
+			k.msg += "built " + bldLabel + "<br/>";
+			bld.click();
+		}
+	},
+	pushTab: function(tab) {
+		k.prevTab = $("a.tab.activeTab")[0].innerText;
+		$('a.tab:contains("' + tab + '")')[0].click();
+	},
+	popTab: function() {
+		$('a.tab:contains("' + k.prevTab + '")')[0].click();
+		k.prevTab = null;
 	}
 };
 
@@ -31,22 +58,33 @@ clearInterval(goi);
 
 var goi = setInterval(function() {
 	console.log("scanning... " + new Date().toTimeString());
+	k.msg = '';
+	
+	// INIT mode - beginning of reset
+	if (gamePage.bld.get('field').val < 50) {
+		$(k.panel).find('#mode').html('INIT');
+		$(k.panel).find('#k-msg').html('');
 
-	// update ui
+		k.build('Catnip field');
+		return;
+	}
+
+	/*
+	if (gamePage.bld.get('pasture').val < 20 &&
+		gamePage.calendar.season < 2) {
+		k.build('Pasture');
+	}*/
+	
+	// TODO: scale k.craftRatio
+	
+	// UI - update mode header
 	var d = new Date();
 	var mode = "[" + d.getHours() + ":" + d.getMinutes() + ":" + ("0" + d.getSeconds()).slice(-2) + "] " + k.mode;
 	$(k.panel).find("#mode").html(mode);
 	
-	var msg = "";
-
-	if (k.mode == "off") {
-		console.log("...disabled");
-		return;
-	}
-    
 	// always observe the sky
 	if ($("input#observeBtn").length == 1) {
-		msg += "sky observed<br/>";
+		k.msg += "sky observed<br/>";
 		console.log("observing the sky");
 		$("input#observeBtn").click();
 	}
@@ -56,28 +94,93 @@ var goi = setInterval(function() {
 		return;
 	}
 
+	// build
     if (k.mode != "nobuild") {	
-		// buildings
 		if ($("a.tab.activeTab")[0].innerText == "Bonfire") {
 			k.buildorder.forEach(function(bldId) {
-				var bld = $(".btnContent:contains(" + bldId.label + ")");
-				if (bld.length == 1 && !bld.parent().hasClass("disabled")) {
-					console.log("building " + bldId.label);
-					msg += "built " + bldId.label + "<br/>";
-					bld.click();
-				}
+				k.build(bldId.label);
 			});
 		}
 	}
 
-	// TODO: toggle to trade with zebras for titanium
+	// find affordable buildings
+	var bldStr = "now building...<br/>";
+	
+    k.buildorder.forEach(function(bld) {
+		if (bld.name.length == 0) return;
+		
+		if (bld.name == "smelter" && k.needs.iron == undefined) return;
+
+		if (!gamePage.bld.getBuildingExt(bld.name).meta.unlocked) {
+			bld.canBuild = false;
+			return;
+		}
+		
+		var prices = gamePage.bld.getPrices(bld.name);
+		bld.canBuild = true;
+		bld.needs = [];
+		prices.forEach(function(price) {
+			var res = gamePage.resPool.resourceMap[price.name];
+			if (res.maxValue > 0 &&
+				price.val > res.maxValue) {
+				bld.canBuild = false;
+			} else if (res.value < price.val) {
+				bld.needs.push({
+					name: price.name,
+					gap: price.val - res.value
+				});
+			}
+		});
+		
+		if (bld.canBuild) {
+			bld.needs.sort(function(a,b) { return a.gap > b.gap ? -1 : 1; })
+			bldStr += bld.label + ": " + bld.needs.map(function(need) {return need.name + " (" + need.gap.toFixed() + ")";}) + "<br/>";
+		}
+	});
+
+	// find affordable science research
+	k.techs = [];
+	gamePage.science.techs.forEach(function(tech) { 
+		if (tech.unlocked && 
+			!tech.researched &&
+			k.canAfford(tech.prices)) {
+			
+			k.techs.push(tech);
+			console.log("Affordable: " + tech.label);
+		}
+	});
+	
+	// calculate aggregate needs
+	// TODO: k.needs should be an array
+	k.needs = [];
+	k.buildorder.forEach(function(b) { 
+		if (b.canBuild && b.needs.length > 0) { 
+			b.needs.forEach(function(n) { 
+				if (k.needs[n.name] == undefined) k.needs[n.name] = 0;
+				k.needs[n.name] += n.gap; 
+			})
+		}
+	});
+
     // hunt
 	if (k.isFull("manpower")) {
-		console.log("hunting");
-		gamePage.huntAll({ preventDefault: function(){}})
+		// TODO: remove k.trade in favor of a toggle or logic
+		if (k.trade) {
+			var zebras = gamePage.diplomacy.get('zebras')
+			if (zebras != undefined && zebras.unlocked && gamePage.resPool.resourceMap.gold.value >= 15) {
+				console.log("trading with zebras");
+				gamePage.diplomacy.tradeAll(zebras);
+			}
+		}
+		if (gamePage.resPool.resourceMap.manpower.value >= 100) {
+			console.log("hunting");
+			gamePage.huntAll({ preventDefault: function(){}})
+		}
 	}
-
+	
 	// religion
+	// TODO: claim religious upgrades
+	// TODO: unicorn sacrifice
 	if (k.isFull("faith")) {
 		console.log("Praise the sun!");
 		gamePage.religionTab.praiseBtn.onClick();
@@ -131,7 +234,7 @@ var goi = setInterval(function() {
 			console.log("crafting parchment");
 			gamePage.craft("parchment", parchmentsPerManuscript);
 		}
-		if (!k.needparchment && 
+		if ($('k-manuscript-toggle').prop('checked') && 
 			gamePage.resPool.resourceMap.parchment.value >= parchmentsPerManuscript) {
 			console.log("crafting manuscript");
 			gamePage.craft("manuscript", 1);
@@ -139,98 +242,102 @@ var goi = setInterval(function() {
 	}
 	
 	if (k.isFull("science")) {
-		if (gamePage.resPool.resourceMap.manuscript.value > 50 && !k.needmanuscript) {
+		if ($('k-compendium-toggle').prop('checked') &&
+			gamePage.resPool.resourceMap.manuscript.value > 50) {
 			console.log("crafting compendium");
 			gamePage.craft("compedium", 1); // typo intentionally copied from game
 		}
-		if ($('#k-blueprint-toggle').prop('checked')) {
+		if ($('#k-blueprint-toggle').prop('checked') &&
+			gamePage.resPool.resourceMap.compedium.value >= 25) 
+		{
+			console.log("crafting blueprint");
 			gamePage.craft('blueprint', 1);
 		}
 	}
-	
+
 	if (k.isFull("titanium") &&
-	    !k.needsteel) {
+	    $('#k-alloy-toggle').prop('checked')) {
 		console.log("crafting alloy");
 		gamePage.craft("alloy", 1);
 	}
 	
-	// update ui
-	var bldStr = "now building...<br/>";
-	
-    k.buildorder.forEach(function(bld) {
-		if (bld.name.length == 0) return;
-		
-		if (!gamePage.bld.getBuildingExt(bld.name).meta.unlocked) {
-			bld.canBuild = false;
-			return;
-		}
-		
-		var prices = gamePage.bld.getPrices(bld.name);
-		bld.canBuild = true;
-		bld.needs = [];
-		prices.forEach(function(price) {
-			var res = gamePage.resPool.resourceMap[price.name];
-			if (res.maxValue > 0 &&
-				price.val > res.maxValue) {
-				bld.canBuild = false;
-			} else if (res.value < price.val) {
-				bld.needs.push({
-					name: price.name,
-					gap: price.val - res.value
-				});
-			}
-		});
-		
-		if (bld.canBuild) {
-			bld.needs.sort(function(a,b) { return a.gap > b.gap ? -1 : 1; })
-			bldStr += bld.label + ": " + bld.needs.map(function(need) {return need.name + " (" + need.gap.toFixed() + ")";}) + "<br/>";
-		}
-	});
-
 	// Assign jobs to jobless kittens based on current needs
-	// TODO: smarter than wood/minerals
-	k.needs = {};
-	k.buildorder.forEach(function(b) { 
-		if (b.canBuild && b.needs.length > 0) { 
-			b.needs.forEach(function(n) { 
-				if (k.needs[n.name] == undefined) k.needs[n.name] = 0;
-				k.needs[n.name] += n.gap; 
-			})
-		}
-	});
-
 	if (gamePage.village.getFreeKittens() > 0) {
-		var jobName = (k.needs.wood > k.needs.minerals || k.needs.minerals == undefined) ? "woodcutter" : "miner";
-		var job = gamePage.village.getJob(jobName);
-		gamePage.village.assignJob(job);
-	} else {
-		// rebalance
-		if ((k.needs.wood > k.needs.minerals || k.needs.minerals == undefined) && 
-			gamePage.village.getJob('miner').value > 15)
-		{
-			this.game.village.sim.removeJob("miner");
-			var job = gamePage.village.getJob("woodcutter");
+		var farmer = gamePage.village.getJob('farmer');
+		if (farmer.value < 2) {
+			// get some farmers at beginning of game
+			console.log('assigning farmer');
+			gamePage.village.assignJob(farmer);
+		} else {
+			// TODO: smarter than wood/minerals
+			var jobName = (k.needs.wood > k.needs.minerals || k.needs.minerals == undefined) ? "woodcutter" : "miner";
+			var job = gamePage.village.getJob(jobName);
+			console.log('assigning ' + jobName);
 			gamePage.village.assignJob(job);
 		}
-		else if ((k.needs.minerals > k.needs.wood || k.needs.woods == undefined) && 
-			gamePage.village.getJob("woodcutter").value > 15)
-		{
+	} else {
+		// rebalance woodcutters and miners
+		if (k.needs.wood == undefined || k.needs.wood < 1) k.needs.wood = 1;
+		if (k.needs.minerals == undefined || k.needs.minerals < 1) k.needs.minerals = 1;
+
+		var needRatio = k.needs.wood / k.needs.minerals;
+		var woodcutter = gamePage.village.getJob("woodcutter");
+		var miner = gamePage.village.getJob("miner");
+		var jobRatio = woodcutter.value / miner.value;
+
+		if (needRatio > jobRatio && miner.value > 1) { 
+			this.game.village.sim.removeJob("miner");
+			console.log('assigning ' + woodcutter.name);
+			gamePage.village.assignJob(woodcutter);
+		} else if (needRatio < jobRatio && woodcutter.value > 1) {
 			this.game.village.sim.removeJob("woodcutter");
-			var job = gamePage.village.getJob("miner");
-			gamePage.village.assignJob(job);
+			console.log('assigning ' + miner.name);
+			gamePage.village.assignJob(miner);
 		}
 	}
 	
-	// TODO: workshop improvements
-	// TODO: Science upgrades
+	// Workshop improvements
+	if (gamePage.workshopTab.visible) {
+		gamePage.workshopTab.buttons.forEach(function(btn) {
+			if (btn.model.metadata.unlocked &&
+				!btn.model.metadata.researched &&
+				k.canAfford(btn.model.prices)) {
+					k.pushTab('Workshop');
+					console.log('upgrading ' + btn.model.name);
+					k.msg += "upgraded " + btn.model.name + "<br/>";
+					btn.buttonContent.click();
+					k.popTab();
+				}
+		});
+	}
+
+	// Science upgrades
+	if (gamePage.libraryTab.visible && k.techs.length > 0) {
+		gamePage.libraryTab.buttons.forEach(function(btn) {
+			if (btn.model.metadata.unlocked && 
+				!btn.model.metadata.researched &&
+				k.canAfford(btn.model.prices)) {
+				k.pushTab('Science');
+				console.log("researching " + btn.model.name);
+				k.msg += "researched " + btn.model.name + "<br/>";
+				btn.buttonContent.click();
+				k.popTab();
+			}
+		});
+	}
 	
-	$(k.panel).find("#k-msg").html(msg);
+	$(k.panel).find("#k-msg").html(k.msg);
 	$(k.panel).find("#k-bld").html(bldStr);
 }, 10000);
 
 
 $("#kcode").remove();
-k.panel = $("<div id='kcode'><div id='mode' /><div id='k-options'><input id='k-blueprint-toggle' name='k-blueprint-toggle' type='checkbox' checked /><label for='k-blueprint-toggle'>make blueprints</label></div><div id='k-msg' /><div id='k-bld' /></div>");
+k.panel = $("<div id='kcode'><div id='mode' /><div id='k-options'>" +
+				"<input id='k-manuscript-toggle' name='k-manuscript-toggle' type='checkbox' checked /><label for='k-manuscript-toggle'>make manuscripts</label><br/>" +
+				"<input id='k-compendium-toggle' name='k-compendium-toggle' type='checkbox' checked /><label for='k-compendium-toggle'>make compendiums</label><br/>" +
+				"<input id='k-blueprint-toggle' name='k-blueprint-toggle' type='checkbox' checked /><label for='k-blueprint-toggle'>make blueprints</label><br/>" +
+				"<input id='k-alloy-toggle' name='k-alloy-toggle' type='checkbox' checked /><label for='k-alloy-toggle'>make alloy</label>" +
+				"</div><div id='k-msg' /><div id='k-bld' /></div>");
 k.panel.append("<style>#kcode { margin-left: 4px; }" +
 				"#kcode #mode::before { color: #808080; content: 'mode: ';}" +
 				"#kcode #k-msg { margin-top: 5px; }" +
